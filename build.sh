9aa94a76f9815dcaed8472263db8b9e44bd575d9
GabrielMorth <95134324+GabrielMorth@users.noreply.github.com>
2023-11-18 09:25:23 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,23 +8,46 @@
 
 
 
-#!/usr/bin/env bash
-# exit on error
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+# command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py; }
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+# # adicione linhas abaixo
+# python manage.py migrate
+
+# # create superuser if missing
+# cat <<EOF | python manage.py shell
+# import os
+# from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+
+# User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+#     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+# EOF
+
+
+
 set -o errexit
-command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py; }
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-# adicione linhas abaixo
-python manage.py migrate
 
-# create superuser if missing
 cat <<EOF | python manage.py shell
 import os
 from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group, Permission
 
 User = get_user_model()
 
 User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+
+g = Group(name="Basic users")
+g.permissions.set([Permission.objects.get(codename=c) for c in ["add_user","view_user","add_list","view_list", "add_comentario", "view_investment"]])
+Group.objects.filter(name="Basic users").exists() or g.save()
 EOF
