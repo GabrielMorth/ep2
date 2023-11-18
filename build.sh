bace51847cccb431013cda9a008a1ee444beb1b0
GabrielMorth <95134324+GabrielMorth@users.noreply.github.com>
2023-11-18 09:20:32 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,13 @@
+# #!/usr/bin/env bash
+# # exit on error
+# set -o errexit
+# command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py; }
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+
+
+
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
@@ -5,3 +15,16 @@ command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
+# adicione linhas abaixo
+python manage.py migrate
+
+# create superuser if missing
+cat <<EOF | python manage.py shell
+import os
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+    User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+EOF
