9cc38fabd1d55f78d9354420d88696a5dedd3f7d
GabrielMorth <gabrielmorth@usp.br>
2023-11-18 14:28:26 -0300

    alterações para deploy no render

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -o errexit
-
+command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py; }
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
