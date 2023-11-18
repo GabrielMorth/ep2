e199e06cfb7363defee83b79caf3282ab2f62f1d
GabrielMorth <95134324+GabrielMorth@users.noreply.github.com>
2023-11-18 14:11:51 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,12 +31,41 @@
 
 
 
+# set -o errexit
+# command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py; }
+# pip install -r requirements.txt
+
+# python manage.py collectstatic --no-input
+
+# python manage.py migrate
+
+# cat <<EOF | python manage.py shell
+# import os
+# from django.contrib.auth import get_user_model
+# from django.contrib.auth.models import Group, Permission
+
+# User = get_user_model()
+
+# User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+#     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
+
+# g = Group(name="Basic users")
+# g.permissions.set([Permission.objects.get(codename=c) for c in ["add_user","view_user","add_list","view_list", "add_comentario", "view_investment"]])
+# Group.objects.filter(name="Basic users").exists() or g.save()
+# EOF
+
+
+
+
+
+
 set -o errexit
-command -v pip || { curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py && python get-pip.py; }
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 
+python manage.py makemigrations
 python manage.py migrate
 
 cat <<EOF | python manage.py shell
@@ -46,10 +75,16 @@ from django.contrib.auth.models import Group, Permission
 
 User = get_user_model()
 
-User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists() or \
+if not User.objects.filter(username=os.environ["DJANGO_SUPERUSER_USERNAME"]).exists():
     User.objects.create_superuser(os.environ["DJANGO_SUPERUSER_USERNAME"], os.environ["DJANGO_SUPERUSER_EMAIL"], os.environ["DJANGO_SUPERUSER_PASSWORD"])
 
-g = Group(name="Basic users")
-g.permissions.set([Permission.objects.get(codename=c) for c in ["add_user","view_user","add_list","view_list", "add_comentario", "view_investment"]])
-Group.objects.filter(name="Basic users").exists() or g.save()
+basic_users_group, _ = Group.objects.get_or_create(name="Basic users")
+
+permissions = ["add_user", "view_user", "add_list", "view_list", "add_comentario", "view_investment"]
+permissions_objs = [Permission.objects.get(codename=c) for c in permissions]
+
+for perm in permissions_objs:
+    basic_users_group.permissions.add(perm)
+
+basic_users_group.save()
 EOF
