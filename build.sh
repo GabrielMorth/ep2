35ac14baab129068336ab9c516928fe12dce9330
GabrielMorth <95134324+GabrielMorth@users.noreply.github.com>
2023-11-17 21:10:46 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # exit on error
 set -o errexit
- 
+command -v pip || { curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python get-pip.py; }
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
